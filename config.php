19a963057a852f86b22ad4ec31402c3e7690bac4
A Chumakov <atoshaxaker@yandex.ru>
2017-05-24 14:57:07 +0300

    Config.php

--- config.php
+++ config.php
@@ -9,6 +9,7 @@
 /**
  * Default config for sandbox API
  */
+//TODO: anton 24.05.17 Убрать конфиг по умолчанию в SignMe.php и в phpDoc
 return [
     'urlSign' => 'https://sandbox.sign.me:443/signapi/sjson',
     'urlCheck' => 'https://sandbox.sign.me:443/signaturecheck/json',
